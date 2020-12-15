cf01daa34f30a72a6142c3f124540da223d75bb1
Thomas Stougaard Lange <thomashlange@gmail.com>
2020-12-15 20:29:15 +0100

    Implement answer buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './questions.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,15 +9,26 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> { // _ means private
+class _MyAppState extends State<MyApp> {
+  // _ means private
   var _questionIndex = 0;
+  var questions = [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["Black", "Green", "Blue", "Red"]
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Dog", "Cow", "Rabbit", "Lion", "Cat"]
+    },
+    {
+      "questionText": "What's your favorite element?",
+      "answers": ["Na", "Au", "He"]
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      "What's your favorite color?",
-      "What's your favorite animal?"
-    ];
     return MaterialApp(
       home: Scaffold(
         // Scaffold: basic app design
@@ -25,19 +37,19 @@ class _MyAppState extends State<MyApp> { // _ means private
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: null,
-            )
-          ],
+            Question(questions[_questionIndex]["questionText"]),
+            ...createAnswerWidgets()
+          ]
         ),
       ),
     );
+  }
+
+  List<Answer> createAnswerWidgets() {
+    return (questions[_questionIndex]["answers"] as List<String>).map((answer)
+          {
+            return Answer(answer, _answerQuestion);
+          }).toList();
   }
 
   void _answerQuestion() {
