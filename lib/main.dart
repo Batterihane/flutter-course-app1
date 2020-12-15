aa587f05732c26fecfc092d986ca6af4f3bf25cf
Thomas Stougaard Lange <thomashlange@gmail.com>
2020-12-15 21:53:45 +0100

    Calculate result

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:first_app/result.dart';
 import 'package:flutter/material.dart';
+
 // import 'package:first_app/quiz.dart'; same as below
 import './quiz.dart';
 import './result.dart';
@@ -14,20 +15,36 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   // _ means private
   var _questionIndex = 0;
+  var _totalScore = 0;
 
   // static const _questions = [ // same as below (unmodifiable list that cannot change)
   final _questions = const [
     {
       "questionText": "What's your favorite color?",
-      "answers": ["Black", "Green", "Blue", "Red"]
+      "answers": [
+        {"text": "Black", "score": 10},
+        {"text": "Green", "score": 5},
+        {"text": "Blue", "score": 3},
+        {"text": "Red", "score": 1}
+      ]
     },
     {
       "questionText": "What's your favorite animal?",
-      "answers": ["Dog", "Cow", "Rabbit", "Lion", "Cat"]
+      "answers": [
+        {"text": "Dog", "score": 10},
+        {"text": "Cow", "score": 6},
+        {"text": "Rabbit", "score": 2},
+        {"text": "Lion", "score": 1},
+        {"text": "Cat", "score": 0}
+      ]
     },
     {
       "questionText": "What's your favorite element?",
-      "answers": ["Na", "Au", "He"]
+      "answers": [
+        {"text": "Na", "score": 10},
+        {"text": "Au", "score": 7},
+        {"text": "He", "score": 1}
+      ]
     },
   ];
 
@@ -45,12 +62,13 @@ class _MyAppState extends State<MyApp> {
                 questions: _questions,
                 questionIndex: _questionIndex,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex++;
     });
--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    return "You did it!\nScore: ${resultScore}";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Your did it!"));
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    ));
   }
 }
